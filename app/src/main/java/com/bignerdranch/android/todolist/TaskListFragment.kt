eef183b604bab1fd2c6a59e239b050021c4524f1
Stize <stize9@gmail.com>
2024-12-20 16:47:27 +0700

    Сommit 2

--- app/src/main/java/com/bignerdranch/android/todolist/TaskListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/TaskListFragment.kt
@@ -14,34 +14,44 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import database.TDLDatabase
 
+
 class TaskListFragment : Fragment(R.layout.fragment_task_list){
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: TaskAdapter
-	private val taskListViewModel: TaskListViewModel by viewModels {
+
+    private val taskListViewModel: TaskListViewModel by viewModels {
         TaskFactory(TDLDatabase.getDatabase(requireContext()).taskDao())
     }
 
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         recyclerView=view.findViewById(R.id.task_recycler_view)
         recyclerView.layoutManager=LinearLayoutManager(context)
+
         adapter = TaskAdapter(mutableListOf())
         recyclerView.adapter = adapter
+
         taskListViewModel.tasks.observe(viewLifecycleOwner){
             tasks->adapter.updateTasks(tasks)
         }
-        taskListViewModel.addTask(content = "MAke", priority = 3)
+        taskListViewModel.getTasks()
+		    //taskListViewModel.addTask(content = "MAke", priority = 2)
     }
+
     private inner class TaskHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
         private lateinit var task: Task
         private val task_content: TextView = itemView.findViewById(R.id.taskContent)
         private val task_circle: FrameLayout = itemView.findViewById(R.id.priorityCircle)
         private val task_num: TextView = itemView.findViewById(R.id.priorityNumber)
+
         init {
             itemView.setOnClickListener(this)
         }
+
         fun bind(task: Task) {
             this.task = task
             task_content.text = task.content
@@ -52,10 +62,12 @@ class TaskListFragment : Fragment(R.layout.fragment_task_list){
             }
             task_num.text = task.priority.toString()
         }
+
         override fun onClick(v: View?) {
             // Обработка клика по задаче
         }
     }
+
     private inner class TaskAdapter(var tasks: MutableList<Task>) : RecyclerView.Adapter<TaskHolder>() {
         override fun onCreateViewHolder(parent : ViewGroup, viewType: Int) : TaskHolder {
             val view = layoutInflater.inflate(R.layout.list_item_task, parent, false)
@@ -72,6 +84,7 @@ class TaskListFragment : Fragment(R.layout.fragment_task_list){
             notifyDataSetChanged()
         }
     }
+
     companion object
     {
         fun newInstance()=TaskListFragment()

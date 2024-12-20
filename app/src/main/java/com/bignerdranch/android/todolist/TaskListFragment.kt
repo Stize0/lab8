1ef8e54d39c66ba6be2f8cf15218396ca7fc86ee
Stize <stize9@gmail.com>
2024-12-20 16:58:28 +0700

    победа

--- app/src/main/java/com/bignerdranch/android/todolist/TaskListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/TaskListFragment.kt
@@ -10,7 +10,6 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import  androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
